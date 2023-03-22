e9b14f5bae0d97905366d49ca9ae6120a279d4e7
Elisha Benjamin <malishben360@gmail.com>
2023-03-23 00:27:28 +0100

    Feature: isAuthenticated middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { merge, get } from 'lodash';
+import { getUserBySessionToken } from 'models/user';
+
+export const isAuthenticated = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['MALISH-AUTH'];
+
+        //Check if it has active session
+        if (!sessionToken) {
+            return res.sendStatus(403);
+        }
+
+        //Get user by session token
+        const existingUser = await getUserBySessionToken(sessionToken);
+
+        //Check if user exist
+        if (!existingUser) {
+            return res.sendStatus(404);
+        }
+
+        merge(req, { identity: existingUser });
+        next();
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
