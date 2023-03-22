e398e738a2ccd3eb674225fd45e5127dd6404743
Elisha Benjamin <malishben360@gmail.com>
2023-03-22 23:51:33 +0100

    Update: User login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,43 @@ import express from 'express';
 import { authentication, random } from '../helpers';
 import { createUser, getUserByEmail } from '../models/user';
 
+export const login = async(req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        //Check if payload contains email and password
+        if (!email || !password) {
+            return res.sendStatus(403);
+        }
+
+        const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
+
+        //Check if user exist
+        if (!user){
+            return res.sendStatus(404);
+        }
+
+        //Authenticate the user using password hashing
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        //Update user sessionToken
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        user.save()
+        
+        //Set session cookie
+        res.cookie('MALISH-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+        return res.status(200).json(user).end();
+    } catch(error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const register = async(req: express.Request, res: express.Response) => {
     try {
         const { username, email, password } = req.body;
--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { register } from '../controllers/authentication';
+import { register, login } from '../controllers/authentication';
 
 //It will take in the application router as a parameter
 export default (router: express.Router) => {
+    router.post('/auth/login', login);
     router.post('/auth/register', register);
 }
